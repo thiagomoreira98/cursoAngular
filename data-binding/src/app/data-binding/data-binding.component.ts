851724eaea08f0a7a7a647c26a07a0baa18a10de
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-09-07 16:57:31 -0300

    aula 12 - two-way data-binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -16,6 +16,13 @@ export class DataBindingComponent implements OnInit {
 
   isMouseOver: boolean = false;
 
+  nome: String = 'abc';
+
+  pessoa: any = {
+    nome: 'def',
+    idade: 20
+  }
+
   getValor(){
     return 1;
   }
